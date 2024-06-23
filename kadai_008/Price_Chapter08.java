eedc6b77927c8d517cc45490bcd9f44c0801068d
Soyms <166920540+Soyms@users.noreply.github.com>
2024-06-23 17:24:06 +0900

    kadai_008をアップロード

---
+++ kadai_008/Price_Chapter08.java
@@ -0,0 +1,37 @@
+package kadai_008;
+
+public class Price_Chapter08 {
+
+	public static void main(String[] args) {
+		int userAge;
+		int serviceCost;
+		
+		userAge = 30;
+		serviceCost = 0;
+		
+		switch(userAge) {
+		case 10:
+			serviceCost = 1000;
+			break;
+		case 20:
+			serviceCost = 2000;
+			break;
+		case 30,40:
+			serviceCost = 3000;
+			break;
+		case 50,60,70 :
+			serviceCost = 4000;
+			break;
+		case 80:
+			serviceCost = 5000;
+			break;
+		default:
+			serviceCost = 500;
+			break;
+		}
+		
+		System.out.println(userAge + "代の料金は" + serviceCost + "円");
+
+	}
+
+}
