1d1b053206ad384bf5bab2677f118beb22fd7a3b
Soyms <166920540+Soyms@users.noreply.github.com>
2024-07-02 18:22:16 +0900

    kadai_17をアップロード

---
+++ kadai_017/KatoIchiro_Chapter17.java
@@ -0,0 +1,15 @@
+package kadai_017;
+
+public class KatoIchiro_Chapter17 extends Kato_Chapter17 {
+	
+	public KatoIchiro_Chapter17() {
+		
+		givenName = "一郎";
+		
+	}
+
+	@Override
+	public void eachIntroduce() {
+        System.out.println("好きな食べ物はリンゴです。");
+    }
+}
---
+++ kadai_017/Kato_Chapter17.java
@@ -0,0 +1,24 @@
+package kadai_017;
+
+public abstract class Kato_Chapter17 {
+	//名字
+	public String familyName = "加藤";
+	//名を表す
+	public String givenName;
+	//住所を表す
+	public String address = "東京都";
+	
+	//共通の紹介メソッド
+    public void commonIntroduce() {
+    	System.out.println("名前：" + familyName + " " + givenName);
+        System.out.println("住所：" + address);  
+        }
+
+    //
+    public abstract void eachIntroduce();
+    
+    public void execIntroduce() {
+        commonIntroduce();
+        eachIntroduce();
+    }
+}
