af9623179cd11dadf8255514181c10d66e255e87
Soyms <166920540+Soyms@users.noreply.github.com>
2024-07-04 16:52:31 +0900

    kadai_20をアップロード

--- kadai_020/DictionaryExec_Chapter21.java
+++ kadai_020/DictionaryExec_Chapter21.java
@@ -8,7 +8,12 @@ public class DictionaryExec_Chapter21 {
     	// 
         HashMap<String,String> dictionary = new HashMap<String,String>();
         
-     // 単語を参照し、意味を出力
+        // 単語と意味を追加
+        dictionary.put("apple", "りんご");
+        dictionary.put("banana", "バナナ");
+        dictionary.put("grape", "ぶどう");
+        
+        // 単語を参照し、意味を出力
         System.out.println("appleの意味は" + dictionary.get("apple"));
         System.out.println("bananaの意味は" + dictionary.get("banana"));
         System.out.println("grapeの意味は" + dictionary.get("grape"));
